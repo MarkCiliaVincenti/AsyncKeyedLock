3ba55369ba1ba374a52454de07fdd91d789be6f7
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2022-10-23 09:54:42 +0200

    Re-enabled non-generic AsyncKeyedLocker

--- AsyncKeyedLock.Tests/Tests.cs
+++ AsyncKeyedLock.Tests/Tests.cs
@@ -72,5 +72,73 @@ namespace AsyncKeyedLock.Tests
 
             Assert.True(valid);
         }
+
+        [Fact]
+        public async Task Test1AtATimeGenerics()
+        {
+            var range = 25;
+            var asyncKeyedLocker = new AsyncKeyedLocker<int>();
+            var concurrentQueue = new ConcurrentQueue<int>();
+
+            var tasks = Enumerable.Range(1, range * 2)
+                .Select(async i =>
+                {
+                    var key = Convert.ToInt32(Math.Ceiling((double)i / 2));
+                    using (await asyncKeyedLocker.LockAsync(key))
+                    {
+                        concurrentQueue.Enqueue(key);
+                        await Task.Delay(100 * key);
+                    }
+                });
+            await Task.WhenAll(tasks.AsParallel());
+
+            bool valid = true;
+            var list = concurrentQueue.ToList();
+
+            for (int i = 0; i < range; i++)
+            {
+                if (list[i] != list[i + range])
+                {
+                    valid = false;
+                    break;
+                }
+            }
+
+            Assert.True(valid);
+        }
+
+        [Fact]
+        public async Task Test2AtATimeGenerics()
+        {
+            var range = 4;
+            var asyncKeyedLocker = new AsyncKeyedLocker<int>(2);
+            var concurrentQueue = new ConcurrentQueue<int>();
+
+            var tasks = Enumerable.Range(1, range * 4)
+                .Select(async i =>
+                {
+                    var key = Convert.ToInt32(Math.Ceiling((double)i / 4));
+                    using (await asyncKeyedLocker.LockAsync(key))
+                    {
+                        concurrentQueue.Enqueue(key);
+                        await Task.Delay((100 * key) + 1000);
+                    }
+                });
+            await Task.WhenAll(tasks.AsParallel());
+
+            bool valid = true;
+            var list = concurrentQueue.ToList();
+
+            for (int i = 0; i < range * 2; i++)
+            {
+                if (list[i] != list[i + (range * 2)])
+                {
+                    valid = false;
+                    break;
+                }
+            }
+
+            Assert.True(valid);
+        }
     }
 }
--- AsyncKeyedLock/AsyncKeyedLocker.cs
+++ AsyncKeyedLock/AsyncKeyedLocker.cs
@@ -8,16 +8,8 @@ namespace AsyncKeyedLock
     /// <summary>
     /// AsyncKeyedLock class, adapted and improved from <see href="https://stackoverflow.com/questions/31138179/asynchronous-locking-based-on-a-key/31194647#31194647">Stephen Cleary's solution</see>.
     /// </summary>
-    public sealed class AsyncKeyedLocker<TKey> : IAsyncKeyedLocker<TKey>
+    public class AsyncKeyedLocker : AsyncKeyedLocker<object>
     {
-        private readonly AsyncKeyedLockerDictionary<TKey> _semaphoreSlims = new AsyncKeyedLockerDictionary<TKey>();
-        internal AsyncKeyedLockerDictionary<TKey> SemaphoreSlims => _semaphoreSlims;
-
-        /// <summary>
-        /// The maximum number of requests for the semaphore that can be granted concurrently. Defaults to 1.
-        /// </summary>
-        public int MaxCount { get; set; }
-
         /// <summary>
         /// Constructor for AsyncKeyedLock.
         /// </summary>
@@ -25,101 +17,6 @@ namespace AsyncKeyedLock
         public AsyncKeyedLocker(int maxCount = 1)
         {
             MaxCount = maxCount;
-        }
-
-        private ReferenceCounter<TKey> GetOrAdd(TKey key)
-        {
-            return SemaphoreSlims.GetOrAdd(key, MaxCount);
-        }
-
-        /// <summary>
-        /// Synchronously lock based on a key
-        /// </summary>
-        /// <param name="key">The key to lock on.</param>
-        /// <returns>A disposable value.</returns>
-        public IDisposable Lock(TKey key)
-        {
-            var referenceCounter = GetOrAdd(key);
-            referenceCounter.SemaphoreSlim.Wait();
-            return new Releaser<TKey>(this, referenceCounter);
-        }
-
-        /// <summary>
-        /// Asynchronously lock based on a key
-        /// </summary>
-        /// <param name="key">The key to lock on.</param>
-        /// <returns>A disposable value.</returns>
-        public async Task<IDisposable> LockAsync(TKey key)
-        {
-            var referenceCounter = GetOrAdd(key);
-            await referenceCounter.SemaphoreSlim.WaitAsync().ConfigureAwait(false);
-            return new Releaser<TKey>(this, referenceCounter);
-        }
-
-        /// <summary>
-        /// Checks whether or not there is a thread making use of a keyed lock.
-        /// </summary>
-        /// <param name="key">The key requests are locked on.</param>
-        /// <returns><see langword="true"/> if the key is in use; otherwise, false.</returns>
-        public bool IsInUse(TKey key)
-        {
-            return SemaphoreSlims.ContainsKey(key);
-        }
-
-        /// <summary>
-        /// Get the number of requests concurrently locked for a given key.
-        /// </summary>
-        /// <param name="key">The key requests are locked on.</param>
-        /// <returns>The number of requests.</returns>
-        [Obsolete("This method should not longer be used as it is confusing with Semaphore terminology. Use <see cref=\"GetCurrentCount\"/> or <see cref=\"GetRemaningCount\"/> instead depending what you want to do.")]
-        public int GetCount(TKey key)
-        {
-            return GetRemainingCount(key);
-        }
-
-        /// <summary>
-        /// Get the number of requests concurrently locked for a given key.
-        /// </summary>
-        /// <param name="key">The key requests are locked on.</param>
-        /// <returns>The number of requests concurrently locked for a given key.</returns>
-        public int GetRemainingCount(TKey key)
-        {
-            lock (SemaphoreSlims)
-            {
-                if (SemaphoreSlims.TryGetValue(key, out var referenceCounter))
-                {
-                    return referenceCounter.ReferenceCount;
-                }
-                return 0;
-            }
-        }
-
-        /// <summary>
-        /// Get the number of remaining threads that can enter the lock for a given key.
-        /// </summary>
-        /// <param name="key">The key requests are locked on.</param>
-        /// <returns>The number of remaining threads that can enter the lock for a given key.</returns>
-        public int GetCurrentCount(TKey key)
-        {
-            return MaxCount - GetRemainingCount(key);
-        }
-
-        /// <summary>
-        /// Forces requests to be released from the semaphore.
-        /// </summary>
-        /// <param name="key">The key requests are locked on.</param>
-        /// <returns><see langword="true"/> if the key is successfully found and removed; otherwise, false.</returns>
-        public bool ForceRelease(TKey key)
-        {
-            lock (SemaphoreSlims)
-            {
-                if (SemaphoreSlims.TryGetValue(key, out var referenceCounter))
-                {
-                    referenceCounter.SemaphoreSlim.Release(referenceCounter.ReferenceCount);
-                    return SemaphoreSlims.TryRemove(key, out _);
-                }
-                return false;
-            }
         }
     }
 }
